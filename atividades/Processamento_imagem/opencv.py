dcee692e0383187cd6d829853ba75e63841a0ed0
leocassiosilva <leocassiosilva1234@gmail.com>
2022-05-27 12:05:38 -0300

    opencv

--- atividades/Processamento_imagem/opencv.py
+++ atividades/Processamento_imagem/opencv.py
@@ -1,6 +1,7 @@
 # Importação das bibliotecas
 import cv2
 # Leitura da imagem com a função imread()
-imagem = cv2.imread('')
+imagem = cv2.imread(
+    '/home/leocassio/PycharmProjects/computacao-cognitiva/atividades/Processamento_imagem/imgs/entrada/entrada.jpg')
 print('Largura em pixels: ', end='')
 print(imagem.shape[1])
